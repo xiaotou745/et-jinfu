01ee48e9f8d85d7d0dc3627adec84384e7d72dd6
ruhuaxiao <ru.huaxiao@etaostars.com>
2016-03-24 12:56:53 +0800

    获取验证码接口

--- etaofinance/wap/src/main/java/com/etaofinance/wap/controllor/UserController.java
+++ etaofinance/wap/src/main/java/com/etaofinance/wap/controllor/UserController.java
@@ -5,7 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.etaofinance.api.service.inter.IMemberService;
@@ -33,10 +36,7 @@ public class UserController {
 	 */
 	@RequestMapping("sendcode")
 	@ResponseBody
-	public  SendCodeResp sendcode(SendCodeReq req) {
-//	 req=new SendCodeReq();
-//		req.setPhoneNo("13241969288");
-//		req.setType(1);
+	public  SendCodeResp sendcode(@RequestBody SendCodeReq req) {
 		return memberService.sendCode(req);
 	}
 	
