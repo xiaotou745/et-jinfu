a556bf798659fca7bf2d58062e6f8d4cb83a0a27
do <xu.hongwei@etao.cn>
2016-04-06 16:47:31 +0800

    ed

--- etaofinance/wap/src/main/java/com/etaofinance/wap/controllor/CommonController.java
+++ etaofinance/wap/src/main/java/com/etaofinance/wap/controllor/CommonController.java
@@ -29,53 +29,27 @@ public class CommonController {
 	}
 
 	/**
-<<<<<<< HEAD
-	 * ��ȡͼƬ��֤
-=======
 	 * 图形验证码֤
->>>>>>> dacafaf1cc3420c281f1b26e3e4730cf0d6759bc
 	 * @param 
 	 * @author ruhuaxiao 
 	 * @date 2016??3??25??16:53:16
 	 * @return
 	 */
 	@RequestMapping("code")
-<<<<<<< HEAD
-	@ApiOperation(value = "ͼƬ��֤��", httpMethod = "POST", 
-	consumes="application/json;charset=UFT-8",produces="application/json;charset=UFT-8",
-	notes = "ͼƬ��֤��")
-=======
->>>>>>> dacafaf1cc3420c281f1b26e3e4730cf0d6759bc
 	public ModelAndView code(int type) {
 		ModelAndView mv = new ModelAndView("user/code");
 		mv.addObject("CodeType", type);
 		return mv;
 	}
 	/**
-<<<<<<< HEAD
-	 * ��ȡ��֤��ӿ�
-=======
 	 * 短信验证码
->>>>>>> dacafaf1cc3420c281f1b26e3e4730cf0d6759bc
 	 * @param req
 	 * @return
 	 */
 	@RequestMapping("sendcode")
 	@ResponseBody
-<<<<<<< HEAD
-	@ApiOperation(value = "������֤��", httpMethod = "POST", 
-	consumes="application/json;charset=UFT-8",produces="application/json;charset=UFT-8",
-	notes = "��ȡ��֤��")
-	public  HttpResultModel<Object> sendcode(@RequestBody SendCodeReq req) {
-		return memberService.sendCode(req);
-	}
-	
-
-	
-=======
 	public  HttpResultModel<Object> sendcode(@RequestBody SendCodeReq req) {
 		return memberService.sendCode(req);
 	}
 
->>>>>>> dacafaf1cc3420c281f1b26e3e4730cf0d6759bc
 }
