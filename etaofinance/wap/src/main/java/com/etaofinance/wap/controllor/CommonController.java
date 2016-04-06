b40190fef30ca8fea05275e2fb787de2c491fe48
ruhuaxiao <ru.huaxiao@etaostars.com>
2016-04-06 16:16:17 +0800

    啊

--- etaofinance/wap/src/main/java/com/etaofinance/wap/controllor/CommonController.java
+++ etaofinance/wap/src/main/java/com/etaofinance/wap/controllor/CommonController.java
@@ -1,33 +1,10 @@
 package com.etaofinance.wap.controllor;
-
-
-
-import java.io.IOException;
-import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-
-
-
-
-
-
-
-
-
-
-
-
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.etaofinance.api.service.inter.IMemberService;
 import com.etaofinance.entity.common.HttpResultModel;
 import com.etaofinance.entity.req.SendCodeReq;
@@ -36,9 +13,9 @@ import com.wordnik.swagger.annotations.ApiOperation;
 
 
 /**
- * ¹«¹²Ä£¿é
+ * ï¿½ï¿½ï¿½ï¿½Ä£ï¿½ï¿½
  * @author hulingbo
- * @date 2016Äê4ÔÂ5ÈÕ16:15:06
+ * @date 2016ï¿½ï¿½4ï¿½ï¿½5ï¿½ï¿½16:15:06
  *
  */
 @Controller
@@ -54,47 +31,27 @@ public class CommonController {
 	}
 
 	/**
-	 * »ñÈ¡Í¼Æ¬ÑéÖ¤
+	 * å›¾å½¢éªŒè¯ç Ö¤
 	 * @param 
 	 * @author ruhuaxiao 
 	 * @date 2016??3??25??16:53:16
 	 * @return
 	 */
 	@RequestMapping("code")
-	@ApiOperation(value = "Í¼Æ¬ÑéÖ¤Âë", httpMethod = "POST", 
-	consumes="application/json;charset=UFT-8",produces="application/json;charset=UFT-8",
-	notes = "Í¼Æ¬ÑéÖ¤Âë")
 	public ModelAndView code(int type) {
 		ModelAndView mv = new ModelAndView("user/code");
 		mv.addObject("CodeType", type);
 		return mv;
 	}
 	/**
-	 * »ñÈ¡ÑéÖ¤Âë½Ó¿Ú
+	 * çŸ­ä¿¡éªŒè¯ç 
 	 * @param req
 	 * @return
 	 */
 	@RequestMapping("sendcode")
 	@ResponseBody
-	@ApiOperation(value = "·¢ËÍÑéÖ¤Âë", httpMethod = "POST", 
-	consumes="application/json;charset=UFT-8",produces="application/json;charset=UFT-8",
-	notes = "»ñÈ¡ÑéÖ¤Âë")
 	public  HttpResultModel<Object> sendcode(@RequestBody SendCodeReq req) {
 		return memberService.sendCode(req);
 	}
-	
-	/**
-	 * »ñÈ¡ÑéÖ¤Âë½Ó¿Ú
-	 * @param req
-	 * @return
-	 */
-	@RequestMapping("sendcode")
-	@ResponseBody
-	@ApiOperation(value = "·¢ËÍÑéÖ¤Âë", httpMethod = "POST", 
-	consumes="application/json;charset=UFT-8",produces="application/json;charset=UFT-8",
-	notes = "»ñÈ¡ÑéÖ¤Âë")
-	public  HttpResultModel<Object> sendcode(@RequestBody SendCodeReq req) {
-		return memberService.sendCode(req);
-	}
-	
+
 }
